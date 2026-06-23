0db1cc726c6ac4cddd50e7cd98081889e49817dc
Jonathan Buttner <jonathan.buttner@elastic.co>
2026-06-22 23:34:20 -0400

    Adding seeds

--- run_forceteki.sh
+++ run_forceteki.sh
@@ -1,10 +1,13 @@
 export PYTHONPATH=/Users/jbuttner/proj/home/open_spiel:$PYTHONPATH
 export PYTHONPATH=/Users/jbuttner/proj/home/open_spiel/build/python:$PYTHONPATH
 export FORCETEKI_PATH=/Users/jbuttner/proj/home/forceteki
+export PYTHONHASHSEED=1
 
 ./venv/bin/python3 open_spiel/python/examples/forceteki_psro.py \
   --game_name=python_forceteki_swu \
   --n_players=2 \
+  --seed=1 \
+  --forceteki_seed=1 \
   --oracle_type=PPO \
   --gpsro_iterations=2 \
   --verbose=True \
